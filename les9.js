aa0f426adfd3dba958c25d9144127379a2c7d5ff
Дарья Самойлова <dasha_samoylova_02@mail.ru>
2023-01-10 09:53:02 +0500

    lesson9

---
+++ les9.js
@@ -0,0 +1,162 @@
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init)
+} else {
+    init()
+}
+
+function init() {
+    const data = {
+        name: 'Каталог товаров',
+        hasChildren: true,
+        items: [
+            {
+                name: 'Мойки',
+                hasChildren: true,
+                items: [
+                    {
+                        name: 'Ulgran1',
+                        hasChildren: true,
+                        items: [
+                            {
+                                name: 'SMT1',
+                                hasChildren: false,
+                                items: []
+                            },
+                            {
+                                name: 'SMT2',
+                                hasChildren: false,
+                                items: []
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Ulgran2',
+                        hasChildren: true,
+                        items: [
+                            {
+                                name: 'SMT3',
+                                hasChildren: false,
+                                items: []
+                            },
+                            {
+                                name: 'SMT4',
+                                hasChildren: false,
+                                items: []
+                            }
+                        ]
+                    }
+                ]
+            },{
+                name: 'Фильтры',
+                hasChildren: true,
+                items: [
+                    {
+                        name: 'Ulgran3',
+                        hasChildren: true,
+                        items: [
+                            {
+                                name: 'SMT5',
+                                hasChildren: false,
+                                items: []
+                            },
+                            {
+                                name: 'SMT6',
+                                hasChildren: false,
+                                items: []
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+
+
+    const items = new ListItems(document.getElementById('list-items'), data)
+
+
+    items.render()
+    items.init()
+
+    /*console.log(items.renderTest(data));*/
+
+    function ListItems(el, data) {
+        this.el = el;
+        this.data = data;
+
+        this.init = function () {
+            const parents = this.el.querySelectorAll('[data-parent]')
+
+            parents.forEach(parent => {
+                const open = parent.querySelector('[data-open]')
+
+                open.addEventListener('click', () => this.toggleItems(parent) )
+            })
+        }
+
+        this.render = function () {
+            this.el.insertAdjacentHTML('beforeend', this.renderParent(this.data))
+        }
+
+        this.renderParent = function (data)
+        {
+            let objectE = '';
+            objectE += '<div class="list-item list-item_open" data-parent>'
+            + this.objectassembly(data)
+            + '<div class="list-item__items">'
+            for (let item of data.items) {
+                if (item.hasChildren) {
+                    objectE += this.renderParent(item);
+                }
+                else{
+                    objectE += this.renderChildren(data);
+                }
+            }
+            objectE += '</div>' + '</div>';
+            return objectE;
+
+            //проверка всех элементов на hasChildren
+            //если hasChildren, то запускаем renderParent
+            //если !hasChildren, то запускаем renderChildren
+            //возвращает рендер родительского элемента
+
+        }
+
+        this.renderChildren = function (data)
+        {
+            let objectt ='<div class="list-item__inner">'
+                +'<div class="list-item__space">'
+                +'</div>'
+                +'<img class="list-item__folder" src="img/folder.png" alt="folder">'
+                +'<span>'
+                + data.name
+                +'</span>'
+                +'</div>';
+            return objectt;
+            //вовзращает рендер элемента без вложенности
+        }
+
+        this.toggleItems = function (parent) {
+            parent.classList.toggle('list-item_open')
+        }
+
+        /*        this.renderTest = function (data) {
+                    return `
+                    <div class="test">${data.name}</div>
+                    `
+                }*/
+
+        this.objectassembly = function (data)
+        {
+            let objectt = '';
+            objectt += '<div class="list-item__inner">'
+            +'<img class="list-item__arrow" src="img/chevron-down.png" alt="chevron-down" data-open>'
+            +'<img class="list-item__folder" src="img/folder.png" alt="folder">'
+            +'<span>'
+            +data.name
+            +'</span>'
+            +'</div>';
+            return objectt;
+        }
+    }
+}
